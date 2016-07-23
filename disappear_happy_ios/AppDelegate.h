2938c9150416ba840c2389df2afa8a1405cc9704
LiliCode <lili2011050508@163.com>
2016-07-23 22:23:16 +0800

    Initial Commit

---
+++ disappear_happy_ios/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  disappear_happy_ios
+//
+//  Created by Lili on 16/7/23.
+//  Copyright © 2016年 李立. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ disappear_happy_ios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  disappear_happy_ios
+//
+//  Created by Lili on 16/7/23.
+//  Copyright © 2016年 李立. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
